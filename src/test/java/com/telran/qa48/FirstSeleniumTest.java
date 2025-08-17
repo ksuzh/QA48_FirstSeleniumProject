0e0600e9dac8406a7badc71baa66e91f6860947a
ksuzh <ksuzha1@gmail.com>
2025-08-17 19:09:25 +0300

    add

---
+++ src/test/java/com/telran/qa48/FirstSeleniumTest.java
@@ -0,0 +1,45 @@
+package com.telran.qa48;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstSeleniumTest {
+    WebDriver driver;
+
+    //before - setUp
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        // maximize browser to window
+        driver.manage().window().maximize();
+
+        // wait for all elements on the site to load
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // driver.get("https://www.tel-ran.com"); // without history
+        driver.navigate().to("https://www.tel-ran.com"); // with history
+        driver.navigate().to("https://www.google.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    //test
+    @Test
+    public void openGoogle(){
+        System.out.println("Google opened");
+    }
+
+    //after - teardown
+    @AfterMethod(enabled = true)
+    public void tearDown(){
+        // driver.quit(); closes all tabs
+        driver.close(); //only current tab
+    }
+
+}
